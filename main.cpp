1db2342ccd2926c8b25a475562a7b44e980f5139
qiedingding <xli100@stevens.edu>
2015-11-23 19:35:22 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Server.cpp : Defines the entry point for the console application.
+//
+#include"client.hh"
+#include"server.hh"
+#include "winsock2.h"
+#pragma comment(lib, "ws2_32.lib")
+#include <iostream>
+using namespace std;
+int main(){
+    Client c(1234);
+}
