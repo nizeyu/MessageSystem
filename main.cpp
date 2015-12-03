1ac81982eecf34ee01db48408736be63baacb7f8
nizeyu <zeyuni827@gmail.com>
2015-12-03 17:36:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-// Server.cpp : Defines the entry point for the console application.
-//
-#include"client.hh"
-#include"server.hh"
-#include "winsock2.h"
-#pragma comment(lib, "ws2_32.lib")
-#include <iostream>
-using namespace std;
 
-int main(){
-    Client c(1234);
-}
