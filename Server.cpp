c44f32b08d369865b6a0629dc2d107557efc02d6
qiedingding <xli100@stevens.edu>
2015-12-03 19:07:37 -0500

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,11 +1,58 @@
+/*********************************************************************************
+  *Copyright    Xuan lI
+  *FileName:  server.cpp
+  *Author:  Xuan Li
+  *Version:  1.0
+  *Date:  2015.12.3
+  *Description:  // The server program can:
+                        1: receive message from client program
+                        2. transfer message from one client to another client
+                        3. receive request from one client and broadcast this message to all client
+                        4. receive file from one client and transfer this steam to another client
+  *Others:  none
+
+**********************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 #include <winsock2.h>
+#include <iostream>
 #pragma comment (lib, "ws2_32.lib")  //load ws2_32.dll
 
 #define BUF_SIZE 100
 
+
+using namespace std;
+// create  receive  message thread
+DWORD WINAPI ClientThread (LPVOID ipParameter)
+    {
+        SOCKET ClientScoket  = (SOCKET)ipParameter;
+        int RET =  0;
+        char RecvBuffer[100];
+         char sendvBuffer[100];
+        while(true){
+        memset(RecvBuffer,0x00,sizeof(RecvBuffer));
+        RET  =  recv(ClientScoket,RecvBuffer,100,0);
+        if(RET == 0||RET == SOCKET_ERROR)
+        {
+            cout<<"failed,exit"<<endl;
+            break;
+        }
+        cout<<"message is "<<RecvBuffer<<endl;
+        printf("Input a string: ");
+        gets(sendvBuffer);
+        send(ClientScoket, sendvBuffer, strlen(sendvBuffer), 0);  //send data to client
+    }
+
+    return 0;
+}
 int main(){
+    printf("==========================================================\n");
+
+    printf("|This program is wirten by Xuan Li, Zeyu Ni, Shuo Zhang  |\n");
+    printf("|This program  using socket lib and tcp/ip portocol      |\n");
+    printf("==========================================================\n");
     WSADATA wsaData;
+       HANDLE      hThread =  NULL;
 
         if(WSAStartup(MAKEWORD(2,2),&wsaData)!=0)
         {
@@ -22,7 +69,7 @@ int main(){
         }
     //bind socket
     sockaddr_in sockAddr;
-    memset(&sockAddr, 0, sizeof(sockAddr));  /
+    memset(&sockAddr, 0, sizeof(sockAddr));
     sockAddr.sin_family = AF_INET;  //using ipv4 adress
     sockAddr.sin_addr.s_addr =INADDR_ANY;
     sockAddr.sin_port = htons(5000);  //set the port
@@ -40,7 +87,8 @@ int main(){
     //revieve the request from client
     SOCKADDR clntAddr;
     int nSize = sizeof(SOCKADDR);
-    char buffer[BUF_SIZE] = {0};  //creat buffer area
+    char buffer[BUF_SIZE] = {0};  //create buffer area
+     cout<<"the server has running"<<endl;
     while(1){
        sAccept = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
        if(sAccept==INVALID_SOCKET)
@@ -49,11 +97,18 @@ int main(){
 			break;
 		}
 		printf("accepted client IP:[%s],port:[%d]\n",inet_ntoa(sockAddr.sin_addr),ntohs(sockAddr.sin_port));
-        int strLen = recv(sAccept, buffer, BUF_SIZE, 0);  //
-        printf("Message form client: %s\n", buffer);
-        printf("Input a string: ");
-        gets(buffer);
-        send(sAccept, buffer, strLen, 0);  //send data to client
+		  hThread  = CreateThread(NULL,0,ClientThread,(LPVOID)sAccept,0,NULL);
+		     if(hThread == NULL)
+        {
+           cout<<"creat thread failed"<<endl;
+           break;
+        }
+       CloseHandle(hThread);
+      //  int strLen = recv(sAccept, buffer, BUF_SIZE, 0);  //
+      //  printf("Message form client: %s\n", buffer);
+      //  printf("Input a string: ");
+       // gets(buffer);
+        //send(sAccept, buffer, strlen(buffer), 0);  //send data to client
 
         closesocket(sAccept);
 //close socket
