fb56dce162309864ef4059ec2c54ac9edfdac7c9
zs65927637 <szhang49@stevens.edu>
2015-12-07 18:24:35 -0500

    Update client in multi-thread.cpp

--- client in multi-thread.cpp
+++ client in multi-thread.cpp
@@ -6,36 +6,26 @@
 
 #define BUF_SIZE 100
 HANDLE hMutex;
-
-
 DWORD WINAPI Rec(LPVOID ipParameter)
 {
 	SOCKET sockClient = (SOCKET)ipParameter;
 	char revBuf[BUF_SIZE];
 	int byte = 0;
-
 	while (1)
 	{
-
 		WaitForSingleObject(hMutex, INFINITE);
-		memset(revBuf, 0, sizeof(revBuf));
+		memset(revBuf, 0, BUF_SIZE);
 		byte = recv(sockClient, revBuf, BUF_SIZE , 0);//???????????  
-       
 		printf("Message form server: %s\n", revBuf);
 		if (byte <= 0)
 		{
 			break;
 		}
-
-		 
 		memset(revBuf, 0, BUF_SIZE);  //reset the buffer area
-		
 		ReleaseMutex(hMutex);
-
 	}
 	printf("sdadad");
 	closesocket(sockClient);//??socket,??????  
-	
 	return 0;
 }
 
@@ -43,7 +33,6 @@ int main() {
 	//initialization dll;
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
-
 	//send request to server program
 	sockaddr_in sockAddr;
 	memset(&sockAddr, 0, sizeof(sockAddr));
@@ -51,13 +40,10 @@ int main() {
 	sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");// the server ip address is localhost
 	sockAddr.sin_port = htons(5000);
 	char sendBuf[BUF_SIZE];
-
-
+SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+		connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
 	while (1) {
 		//create socket
-		SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-		connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
-	
 		HANDLE WINAPI hThread = CreateThread(NULL, 0, Rec, (LPVOID)sock, 0, NULL);
 		if (hThread == NULL)
 		{
@@ -66,16 +52,9 @@ int main() {
 		printf("Input a string: ");
 		gets_s(sendBuf);
 		send(sock, sendBuf, strlen(sendBuf), 0);
-		
 		//get what the user input and send is to server
-
-
-
-
-
 		closesocket(sock);  //close socket
 	}
-
 	WSACleanup();  //stop using dll
 	return 0;
 }
