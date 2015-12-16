6fd104e067999e071de25709d7503455f5f2ef7b
zs65927637 <szhang49@stevens.edu>
2015-12-16 18:28:21 -0500

    Update client in multi-thread.cpp

--- client in multi-thread.cpp
+++ client in multi-thread.cpp
@@ -3,6 +3,8 @@
 #include <windows.h>
 #include <string>
 #include <vector>
+#include <iostream>
+using namespace std;
 #pragma warning(disable:4996)
 #pragma comment(lib, "ws2_32.lib")  //load  ws2_32.dl
 #define BUF_SIZE 100
@@ -16,7 +18,7 @@ DWORD WINAPI Rec(LPVOID ipParameter)
 	{
 		WaitForSingleObject(hMutex, INFINITE);
 		memset(revBuf, 0, BUF_SIZE);
-		byte = recv(sockClient, revBuf, BUF_SIZE , 0);//???????????  
+		byte = recv(sockClient, revBuf, BUF_SIZE, 0);//???????????  
 		printf("Message form server: %s\n", revBuf);
 		if (byte <= 0)
 		{
@@ -42,35 +44,29 @@ int main() {
 	sockAddr.sin_addr.s_addr = inet_addr(IP);// the server ip address is localhost
 	sockAddr.sin_port = htons(5000);
 	char sendBuf[BUF_SIZE];
-    SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);//create socket
+	SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);//create socket
 	connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));//connect to the server
 														  //users choosing the different works
-    HANDLE WINAPI hThread = CreateThread(NULL, 0, Rec, (LPVOID)sock, 0, NULL);//open a thread for receive message from server
-	int n;
-	cout << "Please input a number(1-Send Message to Server; 2-Sent Broadcast Request; 3-Send Files):";
-	cin >> n;
+	
+	
 
 	
-	vector<char*> Namelist  ;
-	cout << "Please Input your username:";//Asking for Inputing your Username
-	gets(sendBuf);//Getting client username from Client and Sending to Server
+	printf( "Please Input your username:");//Asking for Inputing your Username
+	gets_s(sendBuf);//Getting client username from Client and Sending to Server
 	send(sock, sendBuf, strlen(sendBuf), 0);
 	memset(sendBuf, 0, BUF_SIZE);
-	while () {
-		int rec;char bufRecv1[BUF_SIZE] = { 0 };//creating receiving buffer
+		int rec; char bufRecv1[BUF_SIZE];//creating receiving buffer
 		rec = recv(sock, bufRecv1, BUF_SIZE, 0);
-		Namelist.pushback(bufrecv1);
-	}
-	cout << "The usernames who are online now: \n ";
-	for (int i = 0; i<strlen(Namelist); i++) {
-		cout << Namelist[i];
-	}
-	cout << "Choose a name you want to chat with: \n ";
+	printf( "The usernames who are online now: \n ");
 	
+	printf(bufRecv1);
+	cout << endl;
+	printf( "Choose a name you want to chat with: \n ");
+	HANDLE WINAPI hThread = CreateThread(NULL, 0, Rec, (LPVOID)sock, 0, NULL);//open a thread for receive message from server
 	while (1) {
 		printf("Input a string: (input 'quit' to quit)\n ");
 		gets_s(sendBuf);
-		if (sendBuf[0] == 'q'&&sendBuf[1] == 'u'&&sendBuf[2] == 'i'&&sendBuf[3] == 't') {  break; }//user input quit to quit the message system
+		if (sendBuf[0] == 'q'&&sendBuf[1] == 'u'&&sendBuf[2] == 'i'&&sendBuf[3] == 't') { break; }//user input quit to quit the message system
 		send(sock, sendBuf, strlen(sendBuf), 0);//get what the user input and send it to server 
 	}
 	/*the following two close function should happen when the client want to end the connect with the server*/
