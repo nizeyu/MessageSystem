92b74a0e7a1f34251baa937eb4d19476004f6c76
nizeyu <zeyuni827@gmail.com>
2015-12-17 15:24:57 -0500

    Update ClientManagement.h

--- final/client/ClientManagement.h
+++ final/client/ClientManagement.h
@@ -2,63 +2,72 @@
 #define CLIENTMANAGEMENT_H
 
 #include <stdio.h>
-#include <WinSock2.h>
+#include <WinSock2.h>                          //for socket
 #include <windows.h>
-#include <iostream>
+#include <iostream>                            //for input and output
 #include <string>                              // for memset
 #include <stdlib.h>                             // for exit
-#include <vector>
+#include <vector>                              // for Client
 #include"Client.h"
 #pragma warning(disable:4996)
 #pragma comment(lib, "ws2_32.lib")  //loading  ws2_32.dll
 #define BUF_SIZE             1024
+
+/**
+ * @Class：ClientManagement
+ * @description: In this Class, all data object and method object are definited in the Message System. The data object include: IP, port, socket, Client, buffer, thread. The method object in this class can complete the initialization, connection between client and server, users log in, sending message, receiving message, broadcast, closing program, etc. These methods can be realized in "client.cpp".
+ * @author: Xuan Li, Zeyu Ni, Shuo Zhang
+ * @version: 1.0,
+ * @date: 12/3/2015
+ */
 class ClientManagement
 {
 private:
-        char* ipAddress;
-        int port;
-        SOCKET sock;
-        Client clientO;
-        char bufSend[BUF_SIZE];
-        HANDLE WINAPI hThread;
+    char* ipAddress;//object: IP address
+    int port;//object: port numner
+    SOCKET sock;//object in socket type
+    Client clientO;//object of Client class in "Client.h"
+    char bufSend[BUF_SIZE];//object: sending buffer
+    HANDLE WINAPI hThread;//Tread definition
 public:
-        sockaddr_in sockAddr;
-        ClientManagement(int pt,  char* ipAddres);
-        void initialization();
-        void connection();
-        char* login(char* username);
-        void close();
-        void sendMessage(char* userlist);
-        void boradcast();
-        void receiveMessage();
-        virtual ~ClientManagement();
-
-        char* GetipAddress() {
-            return ipAddress;
-            }
-        void SetipAddress(char* val) {
-            ipAddress = val;
-            }
-        int Getport() {
-             return port;
-              }
-        void Setport(int val) {
-            port = val;
-             }
-
-        SOCKET Getsock() {
-            return sock;
-             }
-        void Setsock(SOCKET val) {
-            sock = val;
-            }
-        Client GetClient() {
-            return clientO;
-            }
-        void SetClient(Client val) {
-            clientO = val;
-            }
-
+    sockaddr_in sockAddr;
+    ClientManagement(int pt,  char* ipAddres);//constructor
+    void initialization();//definition initialization method
+    void connection();//definition connection method
+    char* login(char* username);//definition login method
+    void close();//definition close method
+    void sendMessage(char* userlist);
+    void boradcast();//definition boradcast method
+    void receiveMessage();//definition receiveMessage method
+    virtual ~ClientManagement();//definition ~ClientManagement method for stopping chat
+    
+    //function for Get and set ip address
+    char* GetipAddress() {
+        return ipAddress;
+    }
+    void SetipAddress(char* val) {
+        ipAddress = val;
+    }
+    //function for Get and set port
+    int Getport() {
+        return port;
+    }
+    void Setport(int val) {
+        port = val;
+    }
+    //function for Get and set socket
+    SOCKET Getsock() {
+        return sock;
+    }
+    void Setsock(SOCKET val) {
+        sock = val;
+    }
+    //function for Get and set client
+    Client GetClient() {
+        return clientO;
+    }
+    void SetClient(Client val) {
+        clientO = val;
+    }
 };
-
 #endif // CLIENTMANAGEMENT_H
