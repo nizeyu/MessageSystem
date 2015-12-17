ff3c93a6703a273a402705d7de7d32e4325c1056
Xuan Li <xli100@stevens.edu>
2015-12-16 21:02:48 -0500

    Create tools.h

---
+++ final/server/tools.h
@@ -0,0 +1,26 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+class tool{
+private:
+public:
+    int  splitString(const string & strSrc, const std::string& strDelims, vector<string>& strDest)
+    {
+    typedef std::string::size_type ST;
+    string delims = strDelims;
+    std::string STR;
+    if(delims.empty()) delims = "\n\r";
+
+
+    ST pos=0, LEN = strSrc.size();
+    while(pos < LEN ){
+    STR="";
+    while( (delims.find(strSrc[pos]) != std::string::npos) && (pos < LEN) ) ++pos;
+    if(pos==LEN) return strDest.size();
+    while( (delims.find(strSrc[pos]) == std::string::npos) && (pos < LEN) ) STR += strSrc[pos++];
+    if( ! STR.empty() ) strDest.push_back(STR);
+    }
+    return strDest.size();
+    }
+
+};
+#endif // TOOLS_H
